0e2400a3015c9ffd7b9c836b50efec747485f9c1
Karen Payne <Karen.1.Payne@oregon.gov>
2021-03-28 10:32:29 -0700

    documentation

--- WinFormDialogs/Dialogs.cs
+++ WinFormDialogs/Dialogs.cs
@@ -12,14 +12,17 @@ namespace WinFormDialogs
         /// <returns>true or false</returns>
         /// <remarks>DebuggerStepThrough attribute stops the debugger from stepping into this method</remarks>
         [DebuggerStepThrough]
-        public static bool Question(string text)
-        {
-            return (MessageBox.Show(
-                text,
-                Application.ProductName,
-                MessageBoxButtons.YesNo,
-                MessageBoxIcon.Question,
-                MessageBoxDefaultButton.Button2) == DialogResult.Yes);
-        }
+        public static bool Question(string text) => (MessageBox.Show(text, "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes);
+
+        /// <summary>
+        /// Prompt user for a question
+        /// </summary>
+        /// <param name="text">Question text</param>
+        /// <param name="title">Caption for dialog</param>
+        /// <returns>true or false</returns>
+        /// <remarks>DebuggerStepThrough attribute stops the debugger from stepping into this method</remarks>        
+        [DebuggerStepThrough]
+        public static bool Question(string text, string title) => (MessageBox.Show(text, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes);
+
     }
 }
---
+++ WinFormDialogs/readme.md
@@ -0,0 +1,21 @@
+﻿# About
+
+provides two easy too use wrappers for MessageBox to ask a question which defaults to the `No` button and returns a `bool`.
+
+# Usage
+
+Add this `using` statement to where the dialog will be used
+
+```csharp
+using static WinFormDialogs.Dialogs;
+```
+
+Ask a question
+
+```csharp
+if (Question("Want to go for lunch?"))
+{
+    
+}
+```
+
