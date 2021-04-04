73609f668299b8348416a79e58246d592013e656
Karen Payne <Karen.1.Payne@oregon.gov>
2021-04-04 07:16:47 -0700

    refactor

--- CommonLanguageExensions/DateTimeExtensions.cs
+++ CommonLanguageExensions/DateTimeExtensions.cs
@@ -16,18 +16,18 @@ namespace CommonLanguageExtensions
         /// <summary>
         /// Returns passed datetime with zero padding using current culture separators
         /// </summary>
-        /// <param name="d"></param>
+        /// <param name="dt"></param>
         /// <returns></returns>
         /// <remarks>
         /// order of date parts year, month, day which can be changed to say month, day, year
         /// </remarks>
-        public static string ZeroPad(this DateTime d)
+        public static string ZeroPad(this DateTime dt)
         {
             string dateSeparator = CultureInfo.CurrentCulture.DateTimeFormat.DateSeparator;
             string timeSeparator = CultureInfo.CurrentCulture.DateTimeFormat.TimeSeparator;
             
-            var resultShort = $"{d.Year:D2}{dateSeparator}{d.Month:D2}{dateSeparator}{d.Day:D2} {d.Hour:D2}{timeSeparator}{d.Minute:D2}{timeSeparator}{d.Second:D2}";
-            var resultLong = d.Year.ToString("D2") + dateSeparator + d.Month.ToString("D2") + dateSeparator + d.Day.ToString("D2") + " " + d.Hour.ToString("D2") + timeSeparator + d.Minute.ToString("D2") + timeSeparator + d.Second.ToString("D2");
+            var resultShort = $"{dt.Year:D2}{dateSeparator}{dt.Month:D2}{dateSeparator}{dt.Day:D2} {dt.Hour:D2}{timeSeparator}{dt.Minute:D2}{timeSeparator}{dt.Second:D2}";
+            var resultLong = dt.Year.ToString("D2") + dateSeparator + dt.Month.ToString("D2") + dateSeparator + dt.Day.ToString("D2") + " " + dt.Hour.ToString("D2") + timeSeparator + dt.Minute.ToString("D2") + timeSeparator + dt.Second.ToString("D2");
 
             return resultShort;
         }
